71444ea9b1a91955557a7f28852ee624ab8b4c7f
Aleksey Naydenko <aleksey.naydenko@virtualhealth.com>
2014-04-06 22:40:37 +0300

    Added flashvars to flashSWF

--- views/yiippod.php
+++ views/yiippod.php
@@ -21,7 +21,7 @@
     var ua = navigator.userAgent.toLowerCase();
     var flashInstalled = false;
     if (typeof(navigator.plugins)!="undefined" && typeof(navigator.plugins["Shockwave Flash"])=="object") {
-         flashInstalled = true;
+        flashInstalled = true;
     } else if (typeof window.ActiveXObject != "undefined") {
         try {
             if (new ActiveXObject("ShockwaveFlash.ShockwaveFlash")) {
@@ -38,17 +38,20 @@
         setTimeout('function(){this.<?=$this->html5Id?>.Play()}', 10);
         <?php endif; ?>
     } else {
-       if(!flashInstalled){
-           // NO FLASH
-           document.getElementById("<?=$this->id?>").innerHTML="<a href=http://www.adobe.com/go/getflashplayer>Требуется установить Flash-плеер</a>";
-       } else {
+        if(!flashInstalled){
+            // NO FLASH
+            document.getElementById("<?=$this->id?>").innerHTML="<a href=http://www.adobe.com/go/getflashplayer>Требуется установить Flash-плеер</a>";
+        } else {
             try {
-               // FLASH
-               var flashvars = {"file":"<?=$this->video?>"<?=($this->style ? ', "st": "'.$this->style.'"' : '')?>};
-               var params = {bgcolor:"<?=$this->bgcolor?>",  allowFullScreen:"true", allowScriptAccess:"always",id:"<?=$this->id?>"};
-               new swfobject.embedSWF("<?=Yii::app()->request->baseUrl.$this->swfUrl?>", "<?=$this->id?>", "<?=$this->width?>", "<?=$this->height?>", "10.0.0", false, flashvars, params);
+                // FLASH
+                var flashvars = {"file":"<?=$this->video?>", "uid": "<?=$this->id.'"'
+                    .($this->style ? ', "st": "'.$this->style.'"' : '')
+                    .($this->poster ? ', "poster": "'.$this->poster.'"' : '')
+                    .($this->playlist ? ', "pl": "'.$this->playlist.'"' : '')?>};
+                    var params = {bgcolor:"<?=$this->bgcolor?>",  wmode:"window", allowFullScreen:"true", allowScriptAccess:"always", id:"<?=$this->id?>"};
+                new swfobject.embedSWF("<?=Yii::app()->request->baseUrl.$this->swfUrl?>", "<?=$this->id?>", "<?=$this->width?>", "<?=$this->height?>", "10.0.0", false, flashvars, params);
             } catch(e) {
-               alert(e);
+                alert(e);
             }
         }
     }
